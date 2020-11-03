07e913b65b20a7bd1e84a0a77a0a19b172ee5942
Sandali Senarathna <sandalisenarathna@gmail.com>
2020-11-03 15:16:05 +0530

    css

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -27,10 +27,10 @@
                   <!-- Left links -->
                   <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
                   <li class="nav-item">
-                      <a class="nav-link active" aria-current="page" href="#hero-area">Home</a>
+                      <a class="nav-link active" aria-current="page" href="/#hero-area">Home</a>
                   </li>
                   <li class="nav-item">
-                      <a class="nav-link" href="#about">About Us</a>
+                      <a class="nav-link" href="/#about">About Us</a>
                   </li>
                   <li class="nav-item">
                       <a class="nav-link" href="#">Donations</a>
