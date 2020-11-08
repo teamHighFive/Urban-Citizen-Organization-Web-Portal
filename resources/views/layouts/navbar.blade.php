12f1b31ff7bb46a6e42717f1d4518bf7121f0a36
Sandali Senarathna <sandalisenarathna@gmail.com>
2020-11-08 23:38:28 +0530

    Basic structure of landing page done

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,33 +1,85 @@
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark pink scrolling-navbar">
-    <a class="navbar-brand" href="#"><strong>Navbar</strong></a>
+<nav class="navbar fixed-top navbar-expand-lg navbar-light white scrolling-navbar">
+    <span class="navbar-brand">
+        <img src="{{ asset('images/logo.png') }}" height="50" alt="logo">
+    </span>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/">Home</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Features</a>
+                <a class="nav-link" href="/#about">About Us</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
+                <a class="nav-link" href="/">Donations</a>
+            </li>
+            <!-- Navbar dropdown -->
+            <li class="nav-item dropdown">
+                <span
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-toggle="dropdown"
+                aria-expanded="false"
+                >
+                Events
+                </span>
+                <!-- Dropdown menu -->
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="/event-calendar">Event Calendar</a></li>
+                <li><hr class="dropdown-divider" /></li>
+                <li><a class="dropdown-item" href="/online-conferences">Online Conferences</a></li>
+                </ul>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Opinions</a>
+                <a class="nav-link" href="/posts">Blog</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/gallery">Gallery</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/archieves">Archives</a>
             </li>
         </ul>
-        <ul class="navbar-nav nav-flex-icons">
+        <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-                <a class="nav-link"><i class="fab fa-facebook-f"></i></a>
+                <a class="nav-link" href="#">Login</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link"><i class="fab fa-twitter"></i></a>
+                <a class="nav-link" href="#">Register</a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link"><i class="fab fa-instagram"></i></a>
-            </li>
+            {{-- @guest
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                </li>
+                @if (Route::has('register'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </li>
+                @endif
+            @else
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        <img src="{{asset(Auth::user()->avatar)}}" style="height:35px;width:35px;border-radius:50%;margin-right:15px" alt="" >{{ Auth::user()->fname.' '.Auth::user()->mname }} <span class="caret"></span>
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+            @endguest --}}
         </ul>
     </div>
 </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,19 +25,19 @@
 
         <!--Mask-->
         <div id="intro" class="view">
-            <div class="mask rgba-stylish-strong">
+            <div class="mask rgba-white-strong">
                 <div class="container-fluid d-flex align-items-center justify-content-center h-100">
                     <div class="row d-flex justify-content-center text-center">
                         <div class="col-md-10">
                             <!-- Heading -->
-                            <h2 class="display-4 font-weight-bold white-text pt-5 mb-2 animated fadeInUp">Welcome to Urban Citizes Organization</h2>
+                            <h2 class="display-4 font-weight-bold pt-5 mb-2 animated fadeInUp">Welcome to Urban Citizens Organization</h2>
 
                             <!-- Divider -->
-                            <hr class="hr-light animated fadeInUp">
+                            <hr class="hr-dark animated fadeInUp">
 
                             <!-- Description -->
-                            <h4 class="white-text my-4 animated fadeInUp">Let's make Colombo the right type of city!</h4>
-                            <button type="button" class="btn btn-outline-white animated fadeInUp">Join with Us</button>
+                            <h4 class="my-4 animated fadeInUp">Let's make Colombo the right type of city!</h4>
+                            <button type="button" class="btn btn-cyan animated fadeInUp">Join with Us</button>
                         </div>
                     </div>
                 </div>
@@ -48,6 +48,255 @@
     </header>
 
     <main>
+
+        <!--
+        ==================================================
+        About Section Start
+        ================================================== -->
+        <section id="about" class="pt-5 pb-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-sm-8">
+                        <div class="animated fadeInLeft">
+                            <h2>
+                            ABOUT US
+                            </h2>
+                            <p>
+                                <blockquote>
+                                    Our mission is to make Colombo the right type of city!
+                                </blockquote>
+                            </p>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, adipisci voluptatum repudiandae, natus impedit repellat aut officia illum at assumenda iusto reiciendis placeat. Temporibus, vero.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-4">
+                        <div class="animated fadeInRight">
+                            <img src="images/slider.png" width="100%" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!--
+        ==================================================
+        Donations Section Start
+        ================================================== -->
+        <section id="donations" class="pt-5 pb-5 cyan text-white text-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2 class="fadeInDown">DONATIONS</h1>
+                            <div class="row fadeInDown mt-5" style="color:#333333">
+                                <div class="col-md-4">
+                                    <div class="card card-margin">
+                                        <div class="bg-image hover-overlay ripple" data-ripple-color="light">
+                                            <img
+                                                src="https://mdbootstrap.com/img/new/standard/nature/111.jpg"
+                                                class="img-fluid"
+                                            />
+                                            <a href="#!">
+                                            <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
+                                            </a>
+                                        </div>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Card title</h5>
+                                            <p class="card-text">
+                                                Some quick example text to build on the card title and make up the bulk of the
+                                                card's content.
+                                            </p>
+                                            <a href="#!" class="btn btn-primary">Donate</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="card card-margin">
+                                        <div class="bg-image hover-overlay ripple" data-ripple-color="light">
+                                            <img
+                                                src="https://mdbootstrap.com/img/new/standard/nature/111.jpg"
+                                                class="img-fluid"
+                                            />
+                                            <a href="#!">
+                                            <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
+                                            </a>
+                                        </div>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Card title</h5>
+                                            <p class="card-text">
+                                                Some quick example text to build on the card title and make up the bulk of the
+                                                card's content.
+                                            </p>
+                                            <a href="#!" class="btn btn-primary">Donate</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="card card-margin">
+                                        <div class="bg-image hover-overlay ripple" data-ripple-color="light">
+                                            <img
+                                                src="https://mdbootstrap.com/img/new/standard/nature/111.jpg"
+                                                class="img-fluid"
+                                            />
+                                            <a href="#!">
+                                            <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
+                                            </a>
+                                        </div>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Card title</h5>
+                                            <p class="card-text">
+                                                Some quick example text to build on the card title and make up the bulk of the
+                                                card's content.
+                                            </p>
+                                            <a href="#!" class="btn btn-primary">Donate</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="/" class="btn btn-white cyan-text mt-5">More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!--
+        ==================================================
+        Gallery slider Section Start
+        ================================================== -->
+        <section id="gallery" class="pt-5 pb-5 ">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 col-sm-12">
+                        <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+                            <div class="carousel-inner">
+                              <div class="carousel-item active">
+                                <img
+                                  src="https://mdbootstrap.com/img/new/slides/041.jpg"
+                                  class="d-block w-100"
+                                  alt="..."
+                                />
+                              </div>
+                              <div class="carousel-item">
+                                <img
+                                  src="https://mdbootstrap.com/img/new/slides/042.jpg"
+                                  class="d-block w-100"
+                                  alt="..."
+                                />
+                              </div>
+                              <div class="carousel-item">
+                                <img
+                                  src="https://mdbootstrap.com/img/new/slides/043.jpg"
+                                  class="d-block w-100"
+                                  alt="..."
+                                />
+                              </div>
+                            </div>
+                            <a
+                              class="carousel-control-prev"
+                              href="#carouselExampleFade"
+                              role="button"
+                              data-slide="prev"
+                            >
+                              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                              <span class="visually-hidden">Previous</span>
+                            </a>
+                            <a
+                              class="carousel-control-next"
+                              href="#carouselExampleFade"
+                              role="button"
+                              data-slide="next"
+                            >
+                              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                              <span class="visually-hidden">Next</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!--
+        ==================================================
+        Blog Section Start
+        ================================================== -->
+        <section id="donations" class="pt-5 pb-5 cyan text-white text-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2 class="fadeInDown">What's New</h1>
+                            <div class="row fadeInDown mt-5" style="color:#333333">
+                                <div class="col-md-4">
+                                    <div class="card card-margin">
+                                        <div class="bg-image hover-overlay ripple" data-ripple-color="light">
+                                            <img
+                                                src="https://mdbootstrap.com/img/new/standard/nature/111.jpg"
+                                                class="img-fluid"
+                                            />
+                                            <a href="#!">
+                                            <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
+                                            </a>
+                                        </div>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Card title</h5>
+                                            <p class="card-text">
+                                                Some quick example text to build on the card title and make up the bulk of the
+                                                card's content.
+                                            </p>
+                                            <a href="#!" class="btn btn-primary">View Post</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="card card-margin">
+                                        <div class="bg-image hover-overlay ripple" data-ripple-color="light">
+                                            <img
+                                                src="https://mdbootstrap.com/img/new/standard/nature/111.jpg"
+                                                class="img-fluid"
+                                            />
+                                            <a href="#!">
+                                            <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
+                                            </a>
+                                        </div>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Card title</h5>
+                                            <p class="card-text">
+                                                Some quick example text to build on the card title and make up the bulk of the
+                                                card's content.
+                                            </p>
+                                            <a href="#!" class="btn btn-primary">View Post</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="card card-margin">
+                                        <div class="bg-image hover-overlay ripple" data-ripple-color="light">
+                                            <img
+                                                src="https://mdbootstrap.com/img/new/standard/nature/111.jpg"
+                                                class="img-fluid"
+                                            />
+                                            <a href="#!">
+                                            <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
+                                            </a>
+                                        </div>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Card title</h5>
+                                            <p class="card-text">
+                                                Some quick example text to build on the card title and make up the bulk of the
+                                                card's content.
+                                            </p>
+                                            <a href="#!" class="btn btn-primary">View Post</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="/" class="btn btn-white cyan-text mt-5">More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
 
     </main>
 
