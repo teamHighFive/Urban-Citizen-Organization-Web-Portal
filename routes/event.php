d2c6986c4895990cb16ad1c34cbd032f5a6543b8
Sandali Senarathna <sandalisenarathna@gmail.com>
2020-11-10 13:19:18 +0530

    wip

--- routes/event.php
+++ routes/event.php
@@ -1,6 +1,7 @@
 <?php
 //TODO delete me too
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Event\EventController;//Event-Calendar EventController-Dhananjana
 
 /*
 |--------------------------------------------------------------------------
@@ -13,3 +14,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Route to Display eventpage-Event Calendar View
+Route::get('/event-calendar',[EventController::class,'index'] );
+//Route to Add Event to Calendar-view
+Route::get('/addeventurl',[EventController::class,'display'] );
+//Route to dispaly blade-Table view
+Route::get('/displaydata',[EventController::class,'show']);
+//Route to delete event and show
+Route::get('/deleteeventurl',[EventController::class,'show'] );
+
+//EventController edit function route
+Route::get('/event@edit/{id}',[EventController::class,'edit'] );
+//EventController update function route
+Route::put('/event@update/{id}',[EventController::class,'update'] );
+//EventController destroy function route
+Route::get('/event@destroy/{id}',[EventController::class,'destroy'] );
+//EventController store function route
+Route::post('/add',[EventController::class,'store'] );
