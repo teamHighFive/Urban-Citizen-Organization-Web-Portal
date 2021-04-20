b415b15eb67a2f296352f62817c83836890ee1c0
SandaliSenarathne <sandalisenarathna@gmail.com>
2021-04-21 00:25:44 +0530

    Delete pool options and update pool options done

--- resources/views/poll/editpoll.blade.php
+++ resources/views/poll/editpoll.blade.php
@@ -49,18 +49,89 @@
                 </form>
                 <label><strong>Options:</strong></label>
                 @foreach($options as $option)
-                    <div class="form-group mb-3">
-                        <input onKeyDown="onChangeInput(<?php echo($option->id)  ?>)" type="text" name="option[]" class="form-control mb-2 col-sm-7" placeholder="Type Second Option" value="{{$option->option_name}}"Required>
-                        <div class="btn btn-sm btn-danger" onClick="alert('this is delete')">X</div>
-                        <div class="btn btn-sm btn-primary" onClick="alert('this is update')" id="op<?php echo($option->id)  ?>"  style="visibility:hidden">Update</div>
+                    <div class="form-group mb-3" id="optionDiv<?php echo($option->id) ?>">
+                        <input id="opinput<?php echo($option->id) ?>" onKeyDown="onChangeInput(<?php echo($option->id)  ?>)" type="text" name="option[]" class="form-control mb-2 col-sm-7" placeholder="Type Second Option" value="{{$option->option_name}}"Required>
+                        <p style="color:red" id="opErr<?php echo($option->id) ?>"></p>
+                        <div class="btn btn-sm btn-danger"  onClick="deleteOption(<?php echo($option->id) ?>)">X</div>
+                        <div class="btn btn-sm btn-primary" onClick="editOption(<?php  echo($option->id) ?>)" id="op<?php echo($option->id)  ?>"  style="visibility:hidden">Update</div>
                     </div>
                 @endforeach
+
+
+                <div  class="form-group mb-34" id="poolOptions">
+        
+                </div>
+                <!-- //TODO -->
+                <!-- <div class="btn btn-primary btn-sm" onClick="addNewOption()">+</div> -->
                 
               <script>
                   function onChangeInput(op) {
                     var btn = document.getElementById(`op${op}`);
                     btn.style.visibility = "visible";
                   }
+
+                  function deleteOption(op){
+                    var content = document.getElementById(`optionDiv${op}`);
+                    content.innerHTML = "<p style='color:red'>Deleting.....</p>"
+
+                    //ajax start
+                                        var xmlhttp = new XMLHttpRequest();
+                                        xmlhttp.onreadystatechange = function() {
+                                        if (this.readyState === 4 && this.status == 200) {
+                                                content.remove();
+                                                // let data = this.responseText;
+                                                data = JSON.parse(this.responseText);
+                                                console.log(data);
+                                                // console.log(data.clapCount);
+                                                
+                                                // document.getElementById("clapCount").innerHTML  =  data.clapCount;
+                                                
+                                            
+                                            }
+                                        };
+                                        xmlhttp.open("GET", 'http://127.0.0.1:8000/api/delete-option/'+op, true);//generating  get method link
+                                        xmlhttp.send();
+                    //ajax end
+                  }
+
+                  function editOption(op){
+                    text = document.getElementById(`opinput${op}`).value;
+                    err = document.getElementById(`opErr${op}`);
+                    err.innerHTML = "Updating....";
+                    if(text != ""){
+                                        var xmlhttp = new XMLHttpRequest();
+                                        xmlhttp.onreadystatechange = function() {
+                                        if (this.readyState === 4 && this.status == 200) {
+                                                // let data = this.responseText;
+                                                data = JSON.parse(this.responseText);
+                                                console.log(data);
+                                                err.innerHTML = "";
+                                                // console.log(data.clapCount);
+                                                
+                                                // document.getElementById("clapCount").innerHTML  =  data.clapCount;
+                                                
+                                            
+                                            }
+                                        };
+                                        xmlhttp.open("GET", 'http://127.0.0.1:8000/api/edit-option/'+op+'/'+text, true);//generating  get method link
+                                        xmlhttp.send();
+                    }
+                                        
+                  }
+
+          function addNewOption(){
+            poolCount = document.getElementById("poolCount");
+
+            var newcontent = document.createElement('div');
+            newcontent.innerHTML = poolContent(++poolCount.value);
+            var theDiv = document.getElementById("poolOptions");
+            theDiv.appendChild(newcontent);//
+          }
+
+
+          function poolContent(op){
+              return `<div class="form-group mb-4"><input type="text" name="op${op}" class="form-control mb-2 col-sm-7" placeholder="Type  Option" Required></div>`;
+          }
               </script>
                 
                 
