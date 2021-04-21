65fb4c8c87d51bb1541d1a992713677742ecff3a
SandaliSenarathne <sandalisenarathna@gmail.com>
2021-04-21 15:47:10 +0530

    bug fixed - email verification after admin approval byg fixed

--- app/Http/Middleware/CheckApproved.php
+++ app/Http/Middleware/CheckApproved.php
@@ -19,17 +19,27 @@ class CheckApproved
         $user = Auth::user();
         if($user['status'] == '0')
         {
-            $not_active = Auth::user()->status == '0';
             Auth::logout();
 
             if($user['email_verified_at']== null){
                 $message = 'Your verification mail has been sent to your email. Please verify it as soon as possible...';
-            }elseif($not_active == 1) {
+            }else{
                 $message = 'Your account is waiting for our administrator approval. Please check back later. Thank You...';
             }
             return redirect()->route('login')
             ->with('status',$message)
             ->withErrors(['email' => 'Your account Still Not Actived. Please contact Administrator.']);
+        }else if($user['status'] == '1'){
+
+            if($user['email_verified_at']== null){
+                Auth::logout();
+                $message = 'Your verification mail has been sent to your email. Please verify it as soon as possible...';
+
+                return redirect()->route('login')
+                ->with('status',$message)
+                ->withErrors(['email' => 'Your account Still Not Actived. Please contact Administrator.']);
+            }
+
         }
 
         return $next($request);
